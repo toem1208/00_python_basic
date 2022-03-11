98f44e8ef4b992f571e698af75381ace01d0b80b
toem1208 <101071945+toem1208@users.noreply.github.com>
2022-03-11 14:28:04 +1300

    Create 02_variables.py

---
+++ 02_variables.py
@@ -0,0 +1,15 @@
+# code to learn about variables 
+
+# variables can be strings 
+from xml.etree.ElementTree import PI
+
+
+greeting = "hello world"
+
+# they can also be numbers 
+fav_num = 17 
+
+# Below is a constant (the name is in UPPERCASE)
+PI = 3.14
+
+num_2 = 2
